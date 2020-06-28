3ad4b46703335c3c655b676cf20ae54e889e37b3
Yordanov <iliyanyyordanov@gmail.com>
2020-06-28 23:46:51 +0100

    Finished the Intro section

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
 $(document).ready(function(){
     $(".d1").fadeIn(1000);
+    $(".year2016").fadeIn(5000);
+    $(".skills2016").fadeIn(5000);
     $(".d2").delay(4000).fadeIn(1000);
+    $(".year2018").fadeIn(10000);
+    $(".skills2018").fadeIn(10000);
     $(".d3").delay(8000).fadeIn(1000);
+    $(".year2019").fadeIn(25000);
+    $(".skills2019").fadeIn(25000);
     $(".d4").delay(11000).fadeIn(1000);
+    $(".year2020").fadeIn(30000);
+    $(".skills2020").fadeIn(30000);
 })
 $(window).load(function(){
     // PAGE IS FULLY LOADED  
