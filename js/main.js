9eb8e08190a88493e03457a062210ba8cd67e798
Yordanov <iliyanyyordanov@gmail.com>
2020-07-11 06:49:51 +0100

    Animation updates

--- js/main.js
+++ js/main.js
@@ -1,19 +1,23 @@
 $(document).ready(function(){
+    $(".zoom").delay(3000).animate({fontSize: '23px', fontSize: '40px'}, "slow");
+    $(".zoom").animate({fontSize: '28px'}, "slow");
+    $(".zoom").animate({fontSize: '28px', fontSize: '35px'}, "slow");
+    $(".zoom").animate({fontSize: '28px'}, "slow");
     $(".d1").fadeIn(1000);
     $(".year2016").fadeIn(5000);
     $(".skills2016").fadeIn(5000);
     $(".d2").delay(4000).fadeIn(1000);
-    $(".year2018").fadeIn(10000);
-    $(".skills2018").fadeIn(10000);
+    $(".year2018").delay(4000).fadeIn(5000);
+    $(".skills2018").delay(4000).fadeIn(5000);
     $(".d3").delay(8000).fadeIn(1000);
-    $(".year2019").fadeIn(25000);
-    $(".skills2019").fadeIn(25000);
+    $(".year2019").delay(8000).fadeIn(5000);
+    $(".skills2019").delay(8000).fadeIn(5000);
     $(".d4").delay(11000).fadeIn(1000);
-    $(".year2020").fadeIn(30000);
-    $(".skills2020").fadeIn(30000);
+    $(".year2020").delay(11000).fadeIn(5000);
+    $(".skills2020").delay(11000).fadeIn(5000);
     $(".Projects-hidden").hide();
     $("#Projects-button").click(function(){
-        $(".Projects-hidden").fadeToggle("slow");
+        $(".Projects-hidden").slideToggle("slow");
     });
 })
 $(window).load(function(){
